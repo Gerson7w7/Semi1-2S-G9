fcac6650de504a71390f9cc055046d8641ab5516
Gerson Quiroa <3005563490101@ingenieria.usac.edu.gt>
2023-10-26 14:20:01 -0600

    CHAT FUNCIONAL

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,71 +1,16 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
 
-const URL = "wss://vk7tl0t2b0.execute-api.us-east-1.amazonaws.com/production";
-
 const Chat = (props) => {
-  const { user1, user2 } = props;
-  const [chat, setChat] = useState([]);
+  const { chat, user1, user2, onSendPrivateMessage, setChat } = props;
   const [newMessage, setNewMessage] = useState("");
-  const socket = useRef(null);
-  // const user1 = { id: "1", nombre: "Juan" };
-  // const user2 = { id: "2", nombre: "Duki" };
-  // const chatAux = [
-  //   { userId: "1", text: "Hola" },
-  //   { userId: "2", text: "Hola" },
-  //   { userId: "1", text: "Como estas?" },
-  //   { userId: "2", text: "Bien y tu?" },
-  //   { userId: "1", text: "Bien" },
-  //   { userId: "2", text: "Que haces?" },
-  //   { userId: "1", text: "Nada y tu?" },
-  //   { userId: "2", text: "Nada" },
-  //   { userId: "1", text: "Ok" },
-  //   { userId: "2", text: "Ok" },
-  //   { userId: "1", text: "Adios" },
-  //   { userId: "2", text: "Adios" },
-  // ];
-
-  const onSocketOpen = useCallback(() => {
-    socket.current?.send(JSON.stringify({ action: "setRoom", user1, user2 }));
-  }, [user1, user2]);
-
-  const onSocketMessage = useCallback((dataSTR) => {
-    const data = JSON.parse(dataSTR);
-    setChat(data.historial);
-  }, []);
-
-  const onSocketClose = useCallback(() => {
-    console.log("Desconectado");
-  }, []);
-
-  const onSendMessage = useCallback((message, user2) => {
-    socket.current?.send(JSON.stringify({ action: "sendMessage", message, user2 }));
-  }, []);
-
-  const onConnect = useCallback(() => {
-    if (socket.current?.readyState === socket.OPEN) {
-      socket.current = new WebSocket(URL);
-      socket.current.addEventListener("open", onSocketOpen);
-      socket.current.addEventListener("message", (event) => {
-        onSocketMessage(event.data);
-      });
-      socket.current.addEventListener("close", onSocketClose);
-      console.log("Conectado");
-    }
-  }, []);
 
   useEffect(() => {
-    onConnect();
-    return () => {
-      if (socket.current?.readyState === socket.OPEN) {
-        socket.current.close();
-      }
-    };
-  }, [onConnect]); // Dependencias actualizadas
+  }, []); // Dependencias actualizadas
 
   const sendMessage = () => {
-    // Enviar el mensaje al servidor;
-    onSendMessage(newMessage, user2)
+    onSendPrivateMessage(newMessage)
+    setChat(oldArray => [...oldArray, {userId: user2.id, text:newMessage}])
     // Limpiar el área de texto
     setNewMessage("");
   };
--- frontend/src/pages/ChatAmigos/ChatAmigos.js
+++ frontend/src/pages/ChatAmigos/ChatAmigos.js
@@ -1,15 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import Navegacion from "../../components/Navegacion/Navegacion";
 import "./ChatAmigos.css";
 import Chat from "../../components/Chat/Chat";
 
+const URL = "wss://vk7tl0t2b0.execute-api.us-east-1.amazonaws.com/production";
+
 const ChatAmigos = () => {
   const [amigos, setAmigos] = useState([]);
-  const [user1, setUser1] = useState({ id: "", nombre: "" });
-  const [user2, setUser2] = useState({ id: "", nombre: "" });
+  const [user1, setUser1] = useState("");
+  const [user2, setUser2] = useState("");
   const [showChat, setShowChat] = useState(false);
+  const [chat, setChat] = useState([]);
+  const socket = useRef(null);
 
   const ip = "http://localhost:5000";
+
+  const onSocketOpen = useCallback(() => {
+    const name = user1.nombre;
+    socket.current?.send(JSON.stringify({ action: "setName", name }));
+  }, [user1]);
+
+  const onSocketMessage = useCallback(
+    (dataSTR) => {
+      const data = JSON.parse(dataSTR);
+      console.log("respuesta del socket: ", data);
+      if (data.privateMessage) {
+        setChat((oldArray) => [
+          ...oldArray,
+          { user2: user2.id, text: data.privateMessage },
+        ]);
+      } else if (data.systemMessage) {
+        const user = data.systemMessage.split(" ");
+        let userId = {};
+        if (user1.nombre === user[0]) {
+          userId = user1.id;
+        } else {
+          userId = user2.id;
+        }
+        setChat((oldArray) => [...oldArray, { userId: userId, text: data.systemMessage }]);
+      }
+    },
+    [user1, user2]
+  );
+
+  const onSendPrivateMessage = useCallback(
+    (message) => {
+      const to = user2.nombre;
+      socket.current?.send(
+        JSON.stringify({ action: "sendPrivate", message, to })
+      );
+    },
+    [user2]
+  );
+
+  const onConnect = useCallback(() => {
+    if (socket.current?.readyState === socket.OPEN) {
+      socket.current = new WebSocket(URL);
+      socket.current.addEventListener("open", onSocketOpen);
+      socket.current.addEventListener("message", (event) => {
+        onSocketMessage(event.data);
+      });
+      console.log("Conectado");
+    }
+  }, [onSocketOpen, onSocketMessage]);
 
   useEffect(() => {
     const url = `${ip}/agg-sl-friends`;
@@ -28,10 +81,14 @@ const ChatAmigos = () => {
         .then((res) => {
           console.log("res: ", res);
           setAmigos(res.mis_friends);
-          setUser1({ id: res.userid, nombre: res.nombre });
+          setUser1({ id: res.user1.id, nombre: res.user1.nombre });
         });
     };
     fetchData();
+
+    return () => {
+      socket.current?.close();
+    };
   }, []);
 
   const chatear = (id_usuario2, nombre2) => {
@@ -45,7 +102,7 @@ const ChatAmigos = () => {
       <div className="contenido album contenido-amigos">
         <div className="cards-container">
           <div className="cards-inner-container">
-            <div class="card">
+            {/* <div class="card">
               <img
                 src="https://viapais.com.ar/resizer/MddKMHve-vqiG0CWdOn1NSSULUs=/980x640/smart/filters:quality(75):format(webp)/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/F46KZJQDZNHMDIHW67ZTJBL32Y.jpg"
                 class="card-img-top"
@@ -57,7 +114,7 @@ const ChatAmigos = () => {
                   Enviar mensaje
                 </button>
               </div>
-            </div>
+            </div> */}
             {amigos.map((a) => (
               <div class="card">
                 <img src={a.imagen} class="card-img-top" alt="..." />
@@ -66,7 +123,10 @@ const ChatAmigos = () => {
                   <button
                     type="button"
                     class="btn btn-info"
-                    onClick={() => chatear(a.id, a.nombre)}
+                    onClick={() => {
+                      chatear(a.id, a.nombre);
+                      onConnect();
+                    }}
                   >
                     Enviar mensaje
                   </button>
@@ -75,8 +135,15 @@ const ChatAmigos = () => {
             ))}
           </div>
         </div>
-        <Chat />
-        {/* {showChat ? <Chat user1={user1} user2={user2} /> : <div></div>} */}
+        {showChat && (
+          <Chat
+            chat={chat}
+            user1={user1}
+            user2={user2}
+            onSendPrivateMessage={onSendPrivateMessage}
+            setChat={setChat}
+          />
+        )}
       </div>
     </main>
   );
